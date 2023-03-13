e9297bfcc1a24aa24e019ab326cbe64584162dcb
Younggun Park <ygpark0922@gmail.com>
2023-03-13 14:32:23 +0900

    chore: initiazing w/ swc-loader, webpack, yarn berry and react project

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // Production 빌드 시, 리액트 코드 트랜스파일링 할 시작점 설정.
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "[name].[contenthash].js",
+  },
+
+  // webpack Develop 모드 실행 시, 사용될 static 파일들 경로와 관리 방식 설정.
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3000,
+  },
+
+  // swc 연동을 위한 swc-loader 장착.
+  module: {
+    rules: [
+      {
+        test: /\.([jt]sx?)?$/,
+        use: "swc-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  // 번들링된 JS 코드를 html 파일과 매핑 및 주입시키기 위한 플러그인 설정.
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join("./public/index.html"),
+    }),
+  ],
+};
