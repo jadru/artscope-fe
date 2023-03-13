0bb8a602257f84d58acbd57e8a377038906800e8
Younggun Park <ygpark0922@gmail.com>
2023-03-13 16:43:22 +0900

    feat: tab layout change & webpack watch

--- src/components/TabLayout.tsx/index.tsx
+++ src/components/TabLayout.tsx/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
 const TabLayout = () => (
-  <>
+  <div className="w-full h-full flex flex-col">
     <div className="h-16 flex items-center justify-center">
       <p className="font-extrabold">Art Platform</p>
     </div>
     <div>Tab 위치</div>
-  </>
+  </div>
 );
 
 export default TabLayout;
