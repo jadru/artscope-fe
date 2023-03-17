681a258968b1c018b1887596ce726741fa654db1
Younggun Park <ygpark0922@gmail.com>
2023-03-17 19:57:47 +0900

    feat: grid component, member skeleton, upload skeleton

--- src/pages/Upload/index.tsx
+++ src/pages/Upload/index.tsx
@@ -6,7 +6,7 @@ export const Upload: React.FC = () => {
   return (
     <TabLayout>
       <div className={"flex items-center justify-center w-full h-full"}>
-        <form className="sm:w-full md:w-[400px]">
+        <form className="sm:w-full md:w-[400px] flex flex-col space-y-2">
           <input type="file" className="file-input file-input-bordered file-input-primary w-full" multiple />
           <div className="flex flex-row min-w-full overflow-x-scroll">
             {Array.from({ length: 40 }).map((value, index) => (
@@ -17,7 +17,10 @@ export const Upload: React.FC = () => {
               </div>
             ))}
           </div>
-          <input type="text" multiple className="input input-primary" />
+          <textarea className="textarea textarea-primary resize-none" placeholder="Bio"></textarea>
+          <button className="btn btn-primary" type="submit">
+            Submit
+          </button>
         </form>
       </div>
     </TabLayout>
