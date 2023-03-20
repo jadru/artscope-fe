9a029d8394c7d53663f4de127f2e42d6bb669383
Younggun Park <ygpark0922@gmail.com>
2023-03-20 14:43:43 +0900

    lint: solve lint problem

--- src/states/atom/utilAtom.ts
+++ src/states/atom/utilAtom.ts
@@ -1,6 +1,6 @@
 import { atom } from "recoil";
 // eslint-disable-next-line
-import { TabAtomType } from "../../types/atomType";
+import { TabAtomType } from "@/types/atomType";
 
 export const TabAtom = atom<TabAtomType>({
   key: "TabAtom",
