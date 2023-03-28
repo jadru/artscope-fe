3c2d4516cd9cb0a90c5773a66e08af7457f539a4
Younggun Park <ygpark0922@gmail.com>
2023-03-28 23:49:04 +0900

    fix: problem at github action

--- src/__tests__/pages/404.test.tsx
+++ src/__tests__/pages/404.test.tsx
@@ -2,11 +2,11 @@ import { render, screen } from '@testing-library/react';
 
 import NotFoundPage from '@/pages/404';
 
-describe('404', () => {
-  it('renders a heading', () => {
+describe('404 페이지', () => {
+  it('404페이지의 텍스트 확인', () => {
     render(<NotFoundPage />);
 
-    const heading = screen.getByText(/not found/i);
+    const heading = screen.getByText(/페이지를 찾을 수 없습니다/i);
 
     expect(heading).toBeInTheDocument();
   });
