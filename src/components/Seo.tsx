471061add2e8c7380d95814a5147f88fed41cae6
Younggun Park <ygpark0922@gmail.com>
2023-04-03 16:44:05 +0900

    feat: op image change

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -13,7 +13,7 @@ const defaultMeta = {
    * No need to be filled, will be populated with openGraph function
    * If you wish to use a normal image, just specify the path below
    */
-  image: 'https://www.artscope.kr/large-og.png',
+  image: 'https://www.artscope.kr/large-og.jpg',
 };
 
 type SeoProps = {
