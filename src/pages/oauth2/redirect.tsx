09847f3dce094a6936d7c6a9c4e2be107cf976b6
Younggun Park <ygpark0922@gmail.com>
2023-04-04 23:40:54 +0900

    fix: resolve for dependency problem

--- src/pages/oauth2/redirect.tsx
+++ src/pages/oauth2/redirect.tsx
@@ -1,22 +1,35 @@
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { Cookies } from 'react-cookie';
-import Session from 'react-session-api';
 import { toast } from 'react-toastify';
+
+import jxios from '@/utils/jxios';
 
 const RedirectOAuth2 = () => {
   const router = useRouter();
+  const cookies = useMemo(() => new Cookies(), []);
+  const { push } = useRouter();
   useEffect(() => {
-    const cookies = new Cookies();
     try {
-      axios.post('/api/refresh', router.query.token).then((res) => {
-        const token = res.data.accessToken;
-        cookies.set('refreshToken', res.data.refreshToken, {
-          expires: res.data.expiresIn,
+      jxios.post('/api/refresh', router.query.token).then((res) => {
+        const { accessToken, refreshToken } = res.data;
+        const decodedRefreshToken: { exp: number } = jwt_decode(refreshToken);
+        const decodedAccessToken: { auth: string } = jwt_decode(accessToken);
+        cookies.set('refreshToken', refreshToken, {
+          expires: new Date(decodedRefreshToken.exp * 1000),
         });
-        Session.set('token', token);
-        router.push('/').then(() => toast.success('로그인이 완료되었습니다.'));
+        axios.defaults.headers.common[
+          'Authorization'
+        ] = `Bearer ${accessToken}`;
+        push('/').then(() => {
+          toast.success('로그인이 완료되었습니다.');
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          if (decodedAccessToken.auth === 'ROLE_USER')
+            push('/artist/info').then(() => toast('작가 정보를 입력해주세요.'));
+        });
       });
     } catch (err) {
       let message;
@@ -24,7 +37,7 @@ const RedirectOAuth2 = () => {
       else message = String(err);
       toast.error(message);
     }
-  }, [router]);
+  }, [push, cookies, router]);
 
   return <div></div>;
 };
