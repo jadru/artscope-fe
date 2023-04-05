c31830ca88c9a6b444459bf07538d00c478948b9
Younggun Park <ygpark0922@gmail.com>
2023-04-05 22:03:37 +0900

    feat: axios intercept api call w/ token refreshing

--- src/utils/jxios.ts
+++ src/utils/jxios.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { Cookies } from 'react-cookie';
 
 const Jaxios = axios.create({
   withCredentials: true,
@@ -6,5 +8,49 @@ const Jaxios = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+const getRefreshToken = async () => {
+  const cookies = new Cookies();
+  axios
+    .post('/api/refresh', cookies.get('refreshToken'), {
+      withCredentials: false,
+      data: cookies.get('refreshToken'),
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    })
+    .then((res) => {
+      const { accessToken, refreshToken } = res.data;
+      Jaxios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+      const decodedRefreshToken: { exp: number } = jwt_decode(refreshToken);
+      cookies.set('refreshToken', refreshToken, {
+        expires: new Date(decodedRefreshToken.exp * 1000),
+      });
+    })
+    .catch(() => {
+      cookies.remove('refreshToken');
+    });
+};
+
+Jaxios.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    const {
+      config,
+      response: { status },
+    } = err;
+
+    /** 1 */
+    if (status !== 401 || config.sent) {
+      return Promise.reject(err);
+    }
+
+    /** 2 */
+    config.sent = true;
+    await getRefreshToken();
+
+    return axios(config);
+  }
+);
 
 export default Jaxios;
