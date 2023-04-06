ab61894e28e7cb3d6adf7ac631e18d5f9ba756d1
Younggun Park <ygpark0922@gmail.com>
2023-04-06 15:40:51 +0900

    fix: ing oauth cookies save failture

--- src/pages/oauth2/redirect.tsx
+++ src/pages/oauth2/redirect.tsx
@@ -1,21 +1,21 @@
 import jwt_decode from 'jwt-decode';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { useCookies } from 'react-cookie';
+import { Cookies } from 'react-cookie';
 
 const RedirectOAuth2 = () => {
   const { push, query } = useRouter();
-  const [, setCookie] = useCookies(['refreshToken']);
+
   useEffect(() => {
     if (query.token) {
-      push('/').then(() => {
-        const decodedToken: { exp: number } = jwt_decode(query.token as string);
-        setCookie('refreshToken', query.token, {
-          expires: new Date(decodedToken.exp * 1000),
-        });
+      const cookies = new Cookies();
+      const decodedToken: { exp: number } = jwt_decode(query.token as string);
+      cookies.set('refreshToken', query.token, {
+        expires: new Date(decodedToken.exp * 1000),
       });
+      push('/');
     }
-  }, [push, query, setCookie]);
+  }, [push, query]);
 
   return <div></div>;
 };
