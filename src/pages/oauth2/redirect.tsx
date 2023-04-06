7de5b3238223dc6694c4bca61e2ed6366249b4f3
Younggun Park <ygpark0922@gmail.com>
2023-04-06 15:33:22 +0900

    fix: ing oauth cookies save failture

--- src/pages/oauth2/redirect.tsx
+++ src/pages/oauth2/redirect.tsx
@@ -1,47 +1,21 @@
 import jwt_decode from 'jwt-decode';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useCookies } from 'react-cookie';
-import { toast } from 'react-toastify';
-
-import jxios from '@/utils/jxios';
 
 const RedirectOAuth2 = () => {
   const { push, query } = useRouter();
-  const [isTokenRefreshing, setIsTokenRefreshing] = useState(false);
   const [, setCookie] = useCookies(['refreshToken']);
   useEffect(() => {
-    if (query.token && !isTokenRefreshing) setIsTokenRefreshing(true);
-    jxios
-      .post('/api/refresh', query.token, {
-        withCredentials: false,
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-      })
-      .then((res) => {
-        const { accessToken, refreshToken } = res.data;
-        const decodedRefreshToken: { exp: number } = jwt_decode(refreshToken);
-        setCookie('refreshToken', refreshToken, {
-          expires: new Date(decodedRefreshToken.exp * 1000),
+    if (query.token) {
+      push('/').then(() => {
+        const decodedToken: { exp: number } = jwt_decode(query.token as string);
+        setCookie('refreshToken', query.token, {
+          expires: new Date(decodedToken.exp * 1000),
         });
-        jxios.defaults.headers.common[
-          'Authorization'
-        ] = `Bearer ${accessToken}`;
-        push('/').then(() => {
-          toast.success('로그인이 완료되었습니다.');
-          // // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // // @ts-ignore
-          // if (decodedAccessToken.auth === 'ROLE_USER')
-          //   push('/artist/info').then(() => toast('작가 정보를 입력해주세요.'));
-        });
-      })
-      .catch((err) => {
-        toast.error(err.message);
-        push('/').then(() => toast.error('로그인에 실패했습니다.'));
-      })
-      .finally(() => setIsTokenRefreshing(false));
-  }, [isTokenRefreshing, setCookie, push, query]);
+      });
+    }
+  }, [push, query, setCookie]);
 
   return <div></div>;
 };
