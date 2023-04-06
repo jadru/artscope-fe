ff7c8e00769156d508f2d2969eb5c03cfe7ca04d
Younggun Park <ygpark0922@gmail.com>
2023-04-06 16:39:21 +0900

    fix: oAuth twice check resolve

--- src/pages/oauth2/redirect.tsx
+++ src/pages/oauth2/redirect.tsx
@@ -2,18 +2,39 @@ import jwt_decode from 'jwt-decode';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { Cookies } from 'react-cookie';
+import { toast } from 'react-toastify';
+
+import jxios from '@/utils/jxios';
 
 const RedirectOAuth2 = () => {
   const { push, query } = useRouter();
   useEffect(() => {
     if (query.token) {
       const cookies = new Cookies();
-      const decodedToken: { exp: number } = jwt_decode(query.token as string);
-      cookies.set('refreshToken', query.token, {
-        expires: new Date(decodedToken.exp * 1000),
-        path: '/',
-      });
-      push('/');
+      jxios
+        .post('/api/refresh', query.token, {
+          withCredentials: false,
+          data: cookies.get('refreshToken'),
+          headers: {
+            'Content-Type': 'text/plain',
+          },
+        })
+        .then((res) => {
+          const { accessToken, refreshToken } = res.data;
+          jxios.defaults.headers.common[
+            'Authorization'
+          ] = `Bearer ${accessToken}`;
+          const decodedRefreshToken: { exp: number } = jwt_decode(refreshToken);
+          cookies.set('refreshToken', refreshToken, {
+            expires: new Date(decodedRefreshToken.exp * 1000),
+            path: '/',
+          });
+          push('/').then(() => toast.success('로그인이 완료되었습니다.'));
+        })
+        .catch(() => {
+          cookies.remove('refreshToken');
+          push('/login').then(() => toast.warn('로그인이 필요합니다.'));
+        });
     }
   }, [push, query.token]);
 
