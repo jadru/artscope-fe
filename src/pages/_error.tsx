58b07a4ba532b3e50fffbec15565150c314e76e6
Younggun Park <ygpark0922@gmail.com>
2023-04-18 22:10:30 +0900

    feat: advanced ui & fix ui problems

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import * as React from 'react';
 import { RiAlarmWarningFill } from 'react-icons/ri';
 
+import Footer from '@/components/Footer';
 import Seo from '@/components/Seo';
 
 export default function NotFoundPage() {
@@ -10,8 +11,8 @@ export default function NotFoundPage() {
       <Seo templateTitle='Not Found' />
 
       <main>
-        <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
+        <section className='bg-white dark:bg-dark'>
+          <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black dark:text-gray-200'>
             <RiAlarmWarningFill
               size={60}
               className='drop-shadow-glow animate-flicker text-red-500'
@@ -19,9 +20,10 @@ export default function NotFoundPage() {
             <h1 className='mt-8 text-4xl md:text-6xl'>
               페이지를 찾을 수 없습니다
             </h1>
-            <Link className='link mt-4 md:text-lg' href='/'>
+            <Link className='link mt-4 mb-8 md:text-lg' href='/'>
               홈으로 돌아가기
             </Link>
+            <Footer />
           </div>
         </section>
       </main>
