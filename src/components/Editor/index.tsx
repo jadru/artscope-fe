a04751d65882b38e6858ed99b6218f4a8abef1b8
Younggun Park <ygpark0922@gmail.com>
2023-06-07 15:09:57 +0900

    feat: search fix, hotfix for editor, upload problem

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,26 +1,16 @@
 'use client';
 
-import { Color } from '@tiptap/extension-color';
 import Document from '@tiptap/extension-document';
 import { Heading } from '@tiptap/extension-heading';
 import { Placeholder } from '@tiptap/extension-placeholder';
 import { TextStyle } from '@tiptap/extension-text-style';
-import {
-  BubbleMenu as BubbleMenuComponent,
-  EditorContent,
-  isTextSelection,
-  JSONContent,
-  useEditor,
-} from '@tiptap/react';
+import { EditorContent, JSONContent, useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-import Lottie from 'lottie-react';
 import { useRouter } from 'next/router';
 import React, { useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 
 import jxios from '@/utils/jxios';
-
-import UploadAnimation from '../../../public/animation/65316-upload-progress-bar.json';
 
 import { ArtWorkApiRequestType, ArtWorkMediaType, ArtworkType } from '@/types';
 
@@ -51,7 +41,7 @@ const Editor = ({
   thumbnail?: number;
 }) => {
   const CustomDocument = Document.extend({
-    content: 'heading block*',
+    content: 'heading block* paragraph+', // allow headings and paragraphs
   });
   const tagInput = useRef();
   const router = useRouter();
@@ -67,6 +57,7 @@ const Editor = ({
       CustomDocument,
       StarterKit.configure({
         document: false,
+        heading: false,
       }),
       Placeholder.configure({
         placeholder: ({ node }) => {
@@ -78,7 +69,6 @@ const Editor = ({
         },
       }),
       TextStyle,
-      Color,
       Heading.configure({
         levels: [1, 2, 3],
       }),
@@ -145,7 +135,6 @@ const Editor = ({
       (type === 'edit' &&
         handleEditSaveButton(editor.getJSON(), editor.getHTML()));
   };
-  editor && editor.commands.unsetColor();
   const handleEditSaveButton = async (
     contentJSON?: JSONContent,
     contentHTML?: string
@@ -188,178 +177,151 @@ const Editor = ({
 
   const handleCreateSaveButton = async () => {
     if (isUpload || !editor) return;
-    const HTML = editor.getHTML();
-    const JSON = editor.getJSON();
+    try {
+      const HTML = editor.getHTML();
+      const JSONContent = editor.getJSON();
 
-    if (fileUrls.length === 0) {
-      toast.warn('파일을 업로드해주세요.');
-      return;
-    }
-    if (HTML.substring(4, HTML.search('</h1>')).length === 0) {
-      toast.warn('타이틀을 입력해주세요.');
-      return;
-    }
-    if (HTML.substring(HTML.search('</h1>') + 5).length < 8) {
-      toast.warn('설명을 입력해주세요.');
-      return;
-    }
-    let count = 0;
-    fileUrls.map((fileUrl) => {
-      if (fileUrl.mediaType === 'video' || fileUrl.mediaType === 'image') {
-        count += 1;
+      if (fileUrls.length === 0) {
+        toast.warn('파일을 업로드해주세요.');
+        return;
       }
-    });
-    if (count === 0) {
-      toast.warn('썸네일을 업로드해주세요.');
-      setIsUpload(false);
-      return;
-    }
-    if (
-      fileUrls.reduce((acc, cur) => (cur.file ? cur.file.size + acc : acc), 0) /
-        1000000 >
-      100
-    ) {
-      toast.warn('파일 용량이 너무 큽니다.');
-      setIsUpload(false);
-      return;
-    }
-    await setIsUpload(true);
-    const newState = { ...artwork };
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    newState.dto.title = JSON.content[0].content.reduce(
-      (acc, cur) => acc + cur.text,
-      ''
-    );
-    newState.dto.description = HTML.substring(HTML.search('</h1>') + 5);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    newState.dto.tags = tagInput.current.value
-      .split(',')
+      if (HTML.substring(4, HTML.search('</h1>')).length === 0) {
+        toast.warn('타이틀을 입력해주세요.');
+        return;
+      }
+      if (HTML.substring(HTML.search('</h1>') + 5).length < 8) {
+        toast.warn('설명을 입력해주세요.');
+        return;
+      }
+      let count = 0;
+      fileUrls.map((fileUrl) => {
+        if (fileUrl.mediaType === 'video' || fileUrl.mediaType === 'image') {
+          count += 1;
+        }
+      });
+      if (count === 0) {
+        toast.warn('썸네일을 업로드해주세요.');
+        setIsUpload(false);
+        return;
+      }
+      if (
+        fileUrls.reduce(
+          (acc, cur) => (cur.file ? cur.file.size + acc : acc),
+          0
+        ) /
+          1000000 >
+        100
+      ) {
+        toast.warn('파일 용량이 너무 큽니다.');
+        setIsUpload(false);
+        return;
+      }
+      await setIsUpload(true);
+      const newState = { ...artwork };
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      .map((tag) => tag.trim());
-    newState.dto.visible = checkVisible;
-
-    const formData = new FormData();
-    if (fileUrls[thumbnail].mediaType === 'video') {
-      const cover = (await getVideoCover(
+      newState.dto.title = JSONContent.content[0].content.reduce(
+        (acc, cur) => acc + cur.text,
+        ''
+      );
+      newState.dto.description = HTML.substring(HTML.search('</h1>') + 5);
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      newState.dto.tags = tagInput.current.value
+        .split(',')
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        fileUrls[thumbnail].file,
-        1.5
-      )) as Blob;
-      formData.append(
-        'thumbnailFile',
-        new File([cover], 'thumbnail.jpg', { type: 'image/jpeg' })
+        .map((tag) => tag.trim());
+      newState.dto.visible = checkVisible;
+
+      const formData = new FormData();
+      if (fileUrls[thumbnail].mediaType === 'video') {
+        const cover = (await getVideoCover(
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          fileUrls[thumbnail].file,
+          1.5
+        )) as Blob;
+        formData.append(
+          'thumbnailFile',
+          new File([cover], 'thumbnail.jpg', { type: 'image/jpeg' })
+        );
+      } else if (fileUrls[thumbnail].mediaType === 'image') {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        formData.append('thumbnailFile', fileUrls[thumbnail].file);
+      } else {
+        toast.warn('썸네일을 선택해주세요.');
+        setIsUpload(false);
+        return;
+      }
+
+      newState.dto.medias = [];
+      await fileUrls.forEach((media) => {
+        media.mediaType === 'url'
+          ? formData.append(
+              'mediaFiles',
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              new File([media.linkUrl], 'mediaFiles', {
+                type: 'text/plain',
+              })
+            )
+          : // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            formData.append('mediaFiles', media.file);
+        newState.dto.medias.push({
+          mediaType: media.mediaType,
+          description: media.description,
+        });
+      });
+      await formData.append(
+        'dto',
+        new Blob([JSON.stringify(newState.dto)], { type: 'application/json' })
       );
-    } else if (fileUrls[thumbnail].mediaType === 'image') {
+      await jxios
+        .post('/api/artworks', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Accept: 'application/json',
+          },
+        })
+        .then((res) => {
+          setArtwork(initialArtWork);
+          resetAfterUpload && resetAfterUpload();
+          if (res.status === 201) {
+            router
+              .push('/')
+              .then(
+                () =>
+                  toast.success('작품이 업로드되었습니다.') && router.reload()
+              );
+          }
+        })
+        .catch((err) => {
+          toast.error(err.response.data);
+        })
+        .finally(() => {
+          setIsUpload(false);
+        });
+    } catch (err) {
+      setIsUpload(false);
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      formData.append('thumbnailFile', fileUrls[thumbnail].file);
-    } else {
-      toast.warn('썸네일을 선택해주세요.');
-      setIsUpload(false);
-      return;
+      toast.error(err.message);
     }
-
-    newState.dto.medias = [];
-    await fileUrls.forEach((media) => {
-      media.mediaType === 'url'
-        ? formData.append(
-            'mediaFiles',
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            new File([media.linkUrl], 'mediaFiles', {
-              type: 'text/plain',
-            })
-          )
-        : // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          formData.append('mediaFiles', media.file);
-      newState.dto.medias.push({
-        mediaType: media.mediaType,
-        description: media.description,
-      });
-    });
-    await formData.append(
-      'dto',
-      new Blob([JSON.stringify(newState.dto)], { type: 'application/json' })
-    );
-    await jxios
-      .post('/api/artworks', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          Accept: 'application/json',
-        },
-      })
-      .then((res) => {
-        setArtwork(initialArtWork);
-        resetAfterUpload && resetAfterUpload();
-        if (res.status === 201) {
-          router
-            .push('/')
-            .then(
-              () => toast.success('작품이 업로드되었습니다.') && router.reload()
-            );
-        }
-      })
-      .catch((err) => {
-        toast.error(err.response.data);
-      })
-      .finally(() => {
-        setIsUpload(false);
-      });
   };
 
   return (
     <>
       <div className='editor my-4 w-full'>
-        {editor && (
-          <>
-            <BubbleMenuComponent
-              className='bubble-menu'
-              tippyOptions={{ duration: 100 }}
-              editor={editor}
-              shouldShow={(props) => {
-                return !(
-                  props.editor.isActive('heading', { level: 1 }) ||
-                  props.view.hasFocus() ||
-                  (!props.state.doc.textBetween(props.from, props.to).length &&
-                    isTextSelection(props.state.selection)) ||
-                  props.state.selection.empty
-                );
-              }}
-            >
-              <button
-                onClick={() => editor.chain().focus().toggleBold().run()}
-                className={editor.isActive('bold') ? 'is-active' : ''}
-              >
-                Bold
-              </button>
-              <button
-                onClick={() => editor.chain().focus().toggleItalic().run()}
-                className={editor.isActive('italic') ? 'is-active' : ''}
-              >
-                Italic
-              </button>
-              <button
-                onClick={() => editor.chain().focus().toggleStrike().run()}
-                className={editor.isActive('strike') ? 'is-active' : ''}
-              >
-                Strike
-              </button>
-            </BubbleMenuComponent>
-          </>
-        )}
-
         <EditorContent editor={editor} className='h-full w-full p-3' />
         <p className='my-6 w-full text-sm font-bold text-gray-600 dark:text-neutral-400'>
           타이틀 입력 후 <kbd className='kbd kbd-sm'>Enter</kbd> 시 작품 설명을
           입력할 수 있습니다. (<code>Markdown</code> 지원)
         </p>
       </div>
-      <div className='flex flex-col items-stretch space-y-1 rounded-2xl border p-3 md:flex-row md:items-center md:justify-end md:space-y-0 md:space-x-2'>
+      <div className='flex flex-col items-stretch space-y-1 rounded-2xl border p-3 md:flex-row md:items-center md:justify-end md:space-x-2 md:space-y-0'>
         <input
           type='text'
           className='input-bordered input'
@@ -395,22 +357,19 @@ const Editor = ({
         )}
         <button
           onClick={handleSaveButton}
-          className='btn-primary tooltip tooltip-bottom btn'
+          className='btn-primary btn tooltip tooltip-bottom'
           data-tip='작품을 업로드하면 2023 금샘미술관 전시에 공모됩니다.'
+          disabled={isUpload}
         >
-          저장하기
+          {isUpload ? (
+            <>
+              <span className='loading loading-spinner'></span>업로드중
+            </>
+          ) : (
+            '저장하기'
+          )}
         </button>
       </div>
-
-      {isUpload && (
-        <div className='fixed top-0 left-0 z-50 flex h-screen w-screen items-center justify-center bg-white/40 backdrop-blur'>
-          <Lottie
-            animationData={UploadAnimation}
-            className='w-96'
-            loop={false}
-          />
-        </div>
-      )}
     </>
   );
 };
