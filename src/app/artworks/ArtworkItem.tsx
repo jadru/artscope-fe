04bbb71aff15e4fb7529df0f8ec7c593c3d67594
Younggun Park <ygpark0922@gmail.com>
2023-10-10 17:43:56 +0900

    feat: exhibition and navbar add

--- src/app/artworks/ArtworkItem.tsx
+++ src/app/artworks/ArtworkItem.tsx
@@ -8,7 +8,7 @@ import { ArtworkType } from '@/types';
 const ArtworkItem = ({ artwork: aw }: { artwork: ArtworkType }) => (
   <Link href={'/artwork/' + aw.artwork.id} className='group'>
     <Card shadow='sm'>
-      <CardBody className='overflow-visible p-0 group-hover:bg-default-100'>
+      <CardBody className='overflow-visible p-0 transition duration-100 group-hover:bg-default-100'>
         <Image
           src={aw.artwork.thumbnail.mediaUrl}
           alt={aw.artwork.title}
@@ -19,7 +19,7 @@ const ArtworkItem = ({ artwork: aw }: { artwork: ArtworkType }) => (
           height='300'
         />
       </CardBody>
-      <CardFooter className='justify-between space-x-0.5 text-small group-hover:bg-default-100'>
+      <CardFooter className='justify-between space-x-0.5 text-small transition duration-100 group-hover:bg-default-100'>
         <b className='truncate'>{aw.artwork.title}</b>
         <p className='text-default-500'>{aw.artwork.authorName}</p>
       </CardFooter>
---
+++ src/app/exhibitions/page.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { Pagination } from '@nextui-org/react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+import RootLayout from '@/components/RootLayout';
+import Title from '@/components/Title';
+
+import jxios from '@/utils/jxios';
+
+type exhibitionType = {
+  id: number;
+  title: string;
+  description: string;
+  thumbnail: {
+    mediaType: 'image' | 'video';
+    mediaUrl: string;
+  };
+  link: string;
+  startDate: Date;
+  endDate: Date;
+  createdTime: Date;
+  updatedTime: Date | null;
+  author: string;
+};
+
+export default function Exhibitions() {
+  const [data, setData] = useState<exhibitionType[]>([]);
+
+  const fetchExhibition = async () =>
+    await jxios.get('/api/exhibitions').then((res) => res.data);
+  useEffect(() => {
+    fetchExhibition().then((res) => setData(res));
+  }, []);
+  return (
+    <RootLayout>
+      <Title>전시 일정</Title>
+      <div>
+        {data.map((exhibition) => (
+          <div
+            className='group flex cursor-pointer flex-row justify-between group-hover:bg-amber-100'
+            key={exhibition.id}
+          >
+            <div>
+              <h3>{exhibition.title}</h3>
+              <h4>{exhibition.description}</h4>
+              <h5>
+                {new Date(exhibition.startDate).toLocaleString()} ~
+                {new Date(exhibition.endDate).toLocaleString()}
+              </h5>
+            </div>
+            <Image
+              src={exhibition.thumbnail.mediaUrl}
+              alt='thumbnail'
+              width={100}
+              height={100}
+            />
+          </div>
+        ))}
+      </div>
+      <Pagination total={10} className='self-center' />
+    </RootLayout>
+  );
+}
