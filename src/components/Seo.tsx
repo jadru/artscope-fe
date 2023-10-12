8425d3e844e833faeedd8bdee9f76abb0eb34462
Younggun Park <ygpark0922@gmail.com>
2023-10-12 16:09:18 +0900

    feat: opengraph image change

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -18,7 +18,7 @@ const defaultMeta = {
    * No need to be filled, will be populated with openGraph function
    * If you wish to use a normal image, just specify the path below
    */
-  image: NEXT_PUBLIC_ROOT_URL + '/opengraph-image.jpg',
+  image: NEXT_PUBLIC_ROOT_URL + '/opengraph-image.png',
   themeColor: '#FFFFFF',
 };
 
