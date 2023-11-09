8274749eb8b5421e9505e1a4db332c8ce6614804
Younggun Park <ygpark0922@gmail.com>
2023-11-09 15:46:20 +0900

    feat: exhibition logic change

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { load } from '@cronitorio/cronitor-rum';
 import { NextUIProvider } from '@nextui-org/react';
 import {
   QueryCache,
@@ -7,13 +8,16 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
 import useUserHook from '@/hooks/useUser';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   useUserHook();
+  useEffect(() => {
+    load('YOUR_SITE_ID');
+  }, []);
   const [queryClient] = useState(
     new QueryClient({
       queryCache: new QueryCache({
