55c4aeee8b99226e083ec36f8f69d7c936a445b1
Younggun Park <ygpark0922@gmail.com>
2023-11-24 13:22:37 +0900

    chore: github cd ㄴㄷ셔ㅔ

---
+++ scripts/start-fe-dev.sh
@@ -0,0 +1,23 @@
+IMAGE_FILE_PATH="/home/ubuntu/deploy-front/fe-image-dev.txt"
+IMAGE_NAME=$(cat "$IMAGE_FILE_PATH")
+CONTAINER_ENV_PATH="/home/ubuntu/env/.env.fe-dev"
+SERVICE_NAME=art-fe-dev
+
+echo "IMAGE_NAME: $IMAGE_NAME 도커 실행"
+echo "version: '3.8'
+
+services:
+  art-fe-dev:
+    container_name: art-frontend-production-dev
+    image: ${IMAGE_NAME}
+    ports:
+      - 3000:3000
+    env_file:
+      - ${CONTAINER_ENV_PATH}
+
+networks:
+  default:
+    name: art
+    external: true" > docker-compose.yaml
+
+docker-compose up -d $SERVICE_NAME
