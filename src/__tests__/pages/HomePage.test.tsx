ae5dfda7cd0d50073926fe40eb8a423e347796fd
Younggun Park <ygpark0922@gmail.com>
2023-11-25 17:17:44 +0900

    fix: typecheck

--- src/__tests__/pages/HomePage.test.tsx
+++ src/__tests__/pages/HomePage.test.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import FeedRootPage from '@/app/(main)/(feed)/page';
+import FeedRootPage from '@/app/(main)/(list)/(feed)/page';
 
 jest.mock('next/navigation', () => ({
   useRouter() {
