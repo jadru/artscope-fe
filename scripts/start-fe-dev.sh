e3bf2faca55c58b46b56208c673362fda32f4907
Younggun Park <ygpark0922@gmail.com>
2023-11-29 17:34:14 +0900

    chore: apply github action

--- scripts/start-fe-dev.sh
+++ scripts/start-fe-dev.sh
@@ -8,7 +8,7 @@ echo "version: '3.8'
 
 services:
   art-fe-dev:
-    container_name: art-frontend-dev
+    container_name: art-frontend-production-dev
     image: ${IMAGE_NAME}
     ports:
       - 3004:3000
