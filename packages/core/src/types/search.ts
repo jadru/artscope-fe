748effb76b406e2bc3e2f3983980cfeda296003c
Younggun Park <ygpark0922@gmail.com>
2023-12-06 17:25:35 +0900

    feat: 통합검색 세부 사항 및 tab 추가

--- packages/core/src/types/search.ts
+++ packages/core/src/types/search.ts
@@ -5,15 +5,15 @@ import { SingleEventTypeOnList } from '@/types/event';
 import { feedItemType } from '@/types/feed';
 
 export type searchType = {
-  searchArtworks: {
+  searchArtworks?: {
     artworks: DetailedArtworkType[];
     pageInfo: pageInfoType;
   };
-  searchPosts: { posts: feedItemType[]; pageInfo: pageInfoType };
-  searchExhibitions: {
+  searchPosts?: { posts: feedItemType[]; pageInfo: pageInfoType };
+  searchExhibitions?: {
     exhibitions: SingleEventTypeOnList[];
     pageInfo: pageInfoType;
   };
-  searchAgoras: { agoras: AgoraType[]; pageInfo: pageInfoType };
-  pageInfo: pageInfoType;
+  searchAgoras?: { agoras: AgoraType[]; pageInfo: pageInfoType };
+  pageInfo?: pageInfoType;
 };
