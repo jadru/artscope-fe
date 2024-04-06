563b0a438cb46ac41117c3f036f4f3d257a06bc3
Younggun Park <ygpark0922@gmail.com>
2024-04-06 17:57:30 +0900

    feat: 좋아요 추가, 댓글 제작중

---
+++ packages/core/src/app/(main)/(viewer)/article/[id]/ArticleViewerComment.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import CommentForm from '@/app/(main)/(viewer)/article/[id]/CommentForm';
+import { CommentInputs } from '@/app/(main)/(viewer)/article/[id]/commentSchema';
+import ParentComment from '@/app/(main)/(viewer)/article/[id]/ParentComment';
+import { useUser } from '@/states';
+import jxios from '@/utils/jxios';
+
+export default function ArticleViewerComment(props: { id: number }) {
+  const { user, isLogin } = useUser();
+  const onSubmit = (data: CommentInputs) =>
+    jxios.post(`/api/magazines/${data.id}/comments`, data);
+
+  return (
+    <div className='flex flex-col gap-4 items-start justify-end p-6'>
+      <p className='text-2xl'>댓글</p>
+      <CommentForm
+        onSubmit={onSubmit}
+        id={props.id}
+        authorName={user?.name}
+        authorProfileUrl={user?.picture}
+        disabled={!isLogin}
+      />
+      <ParentComment />
+    </div>
+  );
+}
---
+++ packages/core/src/app/(main)/(viewer)/article/[id]/ParentComment.tsx
@@ -0,0 +1,3 @@
+export default function ParentComment() {
+  return <div></div>;
+}
