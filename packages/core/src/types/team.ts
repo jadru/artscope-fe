e4073245e2c785702aab1599c633dcc03033f0ec
Younggun Park <ygpark0922@gmail.com>
2024-04-12 20:30:36 +0900

    feat: 팀 기능 추가

---
+++ packages/core/src/types/team.ts
@@ -0,0 +1,10 @@
+export type TeamDetailType = {
+  id: number;
+  description: string;
+  address: string;
+  profileImage: string;
+  backgroundImage: string;
+  name: string;
+  createdTime: string;
+  updatedTime: string | null;
+};
