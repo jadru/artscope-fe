c84d5b85451177626706097f4a7ed83d6e9f00eb
Younggun Park <jadru@Youngguns-MacBook-Pro.local>
2024-05-11 15:46:59 +0900

    feat: cookie lib 변경

--- packages/core/src/auth/cookieTokenManager.ts
+++ packages/core/src/auth/cookieTokenManager.ts
@@ -1,14 +1,12 @@
-import cookie from 'react-cookies';
+import { setCookie, getCookie, deleteCookie } from 'cookies-next';
 
 import { NEXT_PUBLIC_ROOT_URL } from '@/constant/env';
 
 export const setAccessToken = (accessToken: string, expiresIn: number) =>
-  cookie.save('access-token', accessToken, {
+  setCookie('access-token', accessToken, {
     path: '/',
-    domain: NEXT_PUBLIC_ROOT_URL,
     httpOnly: true,
     secure: true,
-    sameSite: 'lax',
     expires: new Date(Date.now() + expiresIn * 1000 + 9 * 60 * 60 * 1000),
   });
 
@@ -16,23 +14,23 @@ export const setRefreshToken = (
   refreshToken: string,
   refreshExpiresIn: number
 ) =>
-  cookie.save('refresh-token', refreshToken, {
+  setCookie('refresh-token', refreshToken, {
     path: '/',
     expires: new Date(
       Date.now() + refreshExpiresIn * 1000 + 9 * 60 * 60 * 1000
     ),
   });
 
-export const getAccessToken = async () => await cookie.load('access-token');
+export const getAccessToken = () => getCookie('access-token');
 
-export const getRefreshToken = async () => await cookie.load('refresh-token');
+export const getRefreshToken = () => getCookie('refresh-token');
 
 export const removeAccessToken = () =>
-  cookie.remove('access-token', {
+  deleteCookie('access-token', {
     path: '/',
   });
 
 export const removeRefreshToken = () =>
-  cookie.remove('refresh-token', {
+  deleteCookie('refresh-token', {
     path: '/',
   });
