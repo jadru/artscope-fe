0c9851dd67387edf54c947cc8052504dd58ba626
Younggun Park <jadru@Youngguns-MacBook-Pro.local>
2024-05-11 15:46:59 +0900

    feat: cookie lib 변경

--- packages/core/src/app/api/upload/route.ts
+++ packages/core/src/app/api/upload/route.ts
@@ -16,7 +16,8 @@ export async function POST(request: Request) {
     });
     const { url, fields } = await createPresignedPost(client, {
       Bucket: process.env.AWS_BUCKET_NAME!,
-      Key: process.env.S3_PATH + uuidv4() + `.${contentType.split('/')[1]}`,
+      Key:
+        process.env.S3_PATH + '/' + uuidv4() + `.${contentType.split('/')[1]}`,
       Conditions: [
         ['content-length-range', 0, 20971520], // up to 20 MB
         ['starts-with', '$Content-Type', contentType],
@@ -25,7 +26,7 @@ export async function POST(request: Request) {
         acl: 'public-read',
         'Content-Type': contentType,
       },
-      Expires: 3600, // Seconds before the presigned post expires. 3600 by default.
+      Expires: 1800, // Seconds before the presigned post expires. 3600 by default.
     });
 
     return NextResponse.json({
