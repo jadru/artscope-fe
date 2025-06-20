4c55410e560173152412e7a48128cfea08223df3
Younggun Park <jadru@p2ach.ai>
2025-06-21 01:29:02 +0900

    아티클 목록에서 ArticleItem을 ArticleCard로 변경하고, 그리드 레이아웃을 수정하여 열 간격을 추가함.

--- src/app/team/[id]/article-list.tsx
+++ src/app/team/[id]/article-list.tsx
@@ -13,7 +13,7 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 
-import ArticleItem from "@/app/(main)/(list)/ArticleItem";
+import ArticleCard from "@/app/(main)/(list)/article-card";
 import { useUser } from "@/states";
 import jxios from "@/utils/jxios";
 
@@ -68,9 +68,9 @@ export default function MembersArticleList({ username }: { username: string }) {
       )}
       {isSuccess && (
         <>
-          <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4">
+          <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-1 md:gap-2">
             {data?.magazines.map((article) => (
-              <ArticleItem key={article.id} article={article} />
+              <ArticleCard key={article.id} article={article} />
             ))}
           </div>
           <Pagination>
