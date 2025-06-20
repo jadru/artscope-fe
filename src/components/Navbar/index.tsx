f8aa3fed0172aea05c09d64018c71be8cf4f63cb
Younggun Park <jadru@p2ach.ai>
2025-06-21 01:27:39 +0900

    아티클 카드 컴포넌트에 로딩 및 에러 처리 기능 추가: Skeleton 컴포넌트를 사용하여 이미지 로딩 상태를 표시하고, 아티클 카드에서 카테고리 이름을 업데이트함. 페이지 컴포넌트에서 무한 스크롤 기능을 구현하고, 쿼리 방식 변경으로 데이터 페칭 개선.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,127 +1,38 @@
 "use client";
 
 import Link from "next/link";
-import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
-import {
-  NavigationMenu,
-  NavigationMenuList,
-  NavigationMenuLink,
-} from "@/components/ui/navigation-menu";
-import { MenuIcon } from "lucide-react";
+import { useUser } from "@/states";
+import Image from "next/image";
 
 export default function Navbar() {
+  const { isLogin, user } = useUser();
   return (
     <header className="flex h-20 w-screen shrink-0 items-center px-4 md:px-6 lg:px-8 sticky top-0 z-50 bg-background">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline" className="lg:hidden">
-            <MenuIcon className="h-6 w-6" />
-            <h1 className="text-2xl font-bold font-logo text-primary">
-              Artscope
-            </h1>
-            <span className="sr-only">Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <Link href="#" prefetch={false}>
-            <span className="sr-only">Artscope</span>
-          </Link>
-          <div className="grid gap-2 py-6">
-            <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
-              Home
-            </Link>
-            <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
-              About
-            </Link>
-            <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
-              Services
-            </Link>
-            <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
-              Portfolio
-            </Link>
-            <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
-              Contact
-            </Link>
-          </div>
-        </SheetContent>
-      </Sheet>
-      <Link href="/" className="mr-6 hidden lg:flex" prefetch={false}>
-        <h1 className="text-2xl font-bold font-logo text-primary">Artscope</h1>
+      <Link href="/" className="ml-6 md:mr-6 flex" prefetch={false}>
+        <h1 className="text-2xl font-bold font-logo text-primary hover:text-blue-500">
+          Artscope
+        </h1>
       </Link>
-      <NavigationMenu className="hidden lg:flex">
-        <NavigationMenuList>
-          <NavigationMenuLink asChild>
-            <Link
-              href="#"
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              prefetch={false}
-            >
-              Home
-            </Link>
-          </NavigationMenuLink>
-          <NavigationMenuLink asChild>
-            <Link
-              href="#"
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              prefetch={false}
-            >
-              About
-            </Link>
-          </NavigationMenuLink>
-          <NavigationMenuLink asChild>
-            <Link
-              href="#"
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              prefetch={false}
-            >
-              Services
-            </Link>
-          </NavigationMenuLink>
-          <NavigationMenuLink asChild>
-            <Link
-              href="#"
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              prefetch={false}
-            >
-              Portfolio
-            </Link>
-          </NavigationMenuLink>
-          <NavigationMenuLink asChild>
-            <Link
-              href="#"
-              className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-              prefetch={false}
-            >
-              Contact
-            </Link>
-          </NavigationMenuLink>
-        </NavigationMenuList>
-      </NavigationMenu>
-      <div className="ml-auto flex gap-2">
-        <Button variant="outline">로그인</Button>
-        <Button>회원가입</Button>
-      </div>
+      {!isLogin ? (
+        <div className="ml-auto flex gap-2">
+          <Link href="/user/login">
+            <Button variant="outline">로그인</Button>
+          </Link>
+          <Link href="/user/signup">
+            <Button>회원가입</Button>
+          </Link>
+        </div>
+      ) : (
+        <div className="ml-auto flex gap-2">
+          <Link href={`/profile/${user?.username}`}>
+            <Button variant="outline">내 프로필</Button>
+          </Link>
+          <Link href="/editor/dashboard">
+            <Button>대시보드</Button>
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
